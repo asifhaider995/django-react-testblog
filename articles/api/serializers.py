74cddeb1bbf401c46ad02797d6e84b9038074ba9
asifhaider995 <asif.haider995@gmail.com>
2020-08-08 01:19:14 +0600

    serializer field

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from articles.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.Field(source='user.username')
     class Meta:
         model = Article
         # fields = '__all__'
